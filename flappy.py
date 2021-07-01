2dea47b9990a43b019bf9e88005b075126224b01
Ahmad <80991074+Ahmd-M@users.noreply.github.com>
2021-07-01 16:48:43 -0700

    Update flappy.py

--- flappy.py
+++ flappy.py
@@ -18,7 +18,7 @@ STAT_FONT = pygame.font.SysFont("comicsans", 50)
 END_FONT = pygame.font.SysFont("comicsans", 70)
 
 GRAVITY = 1/4
-FPS = 1200
+FPS = 120
 RELATIVE_PERCENT=.75
 BASE_Y = 900*RELATIVE_PERCENT
 SCREEN_SIZE = (WIDTH,HEIGHT) = re_size((576,1024))
@@ -65,16 +65,21 @@ class Bird(object):
 PIPES = [PIPE_IMG,pygame.transform.flip(PIPE_IMG, False, True)]
 PIPES_HEIGHT = [value*RELATIVE_PERCENT for value in [400,600,800]]
 score = 0
+PIPE_VEL = 0
+GAP = 0
 
 class Pipe(object):
+    global PIPE_VEL, GAP
+    PIPE_VEL = 5*RELATIVE_PERCENT
+    GAP = 300*RELATIVE_PERCENT
     def __init__(self,x):
         self.x = x
-        self.vel = 5*RELATIVE_PERCENT
-        self.height = random.randrange(int(400*RELATIVE_PERCENT), int(HEIGHT-200*RELATIVE_PERCENT))
-        self.bottomY = self.height + 300*RELATIVE_PERCENT
-        self.topY = self.height - PIPE_IMG.get_height()
-        self.bottomRect = PIPES[0].get_rect(center = (self.x+PIPE_IMG.get_width()//2,self.bottomY))
-        self.topRect = PIPES[1].get_rect(center = (self.x+PIPE_IMG.get_width()//2,self.topY))
+        self.minHeight = 100*RELATIVE_PERCENT
+        self.maxHeight = self.minHeight + GAP
+        self.height = random.randrange(int(self.minHeight), int(BASE_Y-(self.maxHeight)))
+        self.topY = self.height - PIPE_IMG.get_height()//2
+        self.bottomY = self.topY + PIPE_IMG.get_height() + GAP
+        self.rects()
         
         
     def draw(self):
@@ -83,7 +88,7 @@ class Pipe(object):
         SCREEN.blit(PIPES[1],self.rects()[1])
 
     def move(self):
-        self.x -= self.vel
+        self.x -= PIPE_VEL
         if self.x+PIPE_IMG.get_width()<0:
             global pipe,score
             pipe = Pipe(WIDTH)
@@ -113,17 +118,14 @@ def main(genomes,config):
         genome.fitness = 0  # start with fitness level of 0
         net = neat.nn.FeedForwardNetwork.create(genome, config)
         nets.append(net)
-        birds.append(Bird(100*RELATIVE_PERCENT,random.randint(0,HEIGHT//2)))
+        birds.append(Bird(100*RELATIVE_PERCENT,HEIGHT//2))
         ge.append(genome)
     
-    baseX = 0
-    score = 0
+    baseX = score = 0
     previous_score = score
-
-    canScore = True
-    running = True
+    canScore = running =True
     
-    while running and len(birds)>0:
+    while running and len(birds):
         SCREEN.blit(BG_IMG,(0,0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -132,7 +134,7 @@ def main(genomes,config):
 
         for x, bird in enumerate(birds):
             ge[x].fitness += 0.1
-            output = nets[birds.index(bird)].activate((bird.y, abs(bird.y - pipe.topY), abs(bird.y - pipe.bottomY)))
+            output = nets[birds.index(bird)].activate((bird.y, abs(bird.y - pipe.topRect.bottom), abs(bird.y - pipe.bottomRect.top)))
             if output[0] > 0.5:
                 bird.jump()
             
@@ -150,21 +152,22 @@ def main(genomes,config):
             canScore=True
 
         #Collision check
-        pipes = [pipe.topRect,pipe.bottomRect]
+        pipes = pipe.rects()
         for bird in birds:
             if bird.rect.colliderect(pipes[0]) or bird.rect.colliderect(pipes[1]):
                 ge[birds.index(bird)].fitness -= 1
                 nets.pop(birds.index(bird))
                 ge.pop(birds.index(bird))
                 birds.pop(birds.index(bird))
-
-            if len(birds)>0:
-                if not BIRD_IMGS[0].get_height()<bird.rect.bottom<BASE_Y:
-                    ge[birds.index(bird)].fitness -= 1
-                    nets.pop(birds.index(bird))
-                    ge.pop(birds.index(bird))
-                    birds.pop(birds.index(bird))
+        for bird in birds:
+            if not BIRD_IMGS[0].get_height()<bird.rect.bottom<BASE_Y:
+                ge[birds.index(bird)].fitness -= 1
+                nets.pop(birds.index(bird))
+                ge.pop(birds.index(bird))
+                birds.pop(birds.index(bird))
         
+        if not len(birds):
+            pipe.x = WIDTH   
 
         if score>previous_score:
             previous_score = score
@@ -183,13 +186,16 @@ def main(genomes,config):
         SCREEN.blit(score_label, (WIDTH - score_label.get_width() - 15, 10))
 
         # generations
-        score_label = STAT_FONT.render("Gens: " + str(gen-1),1,(255,255,255))
-        SCREEN.blit(score_label, (10, 10))
+        gens_label = STAT_FONT.render("Gens: " + str(gen-1),1,(255,255,255))
+        SCREEN.blit(gens_label, (10, 10))
 
         # alive
-        score_label = STAT_FONT.render("Alive: " + str(len(birds)),1,(255,255,255))
-        SCREEN.blit(score_label, (10, 50))
+        alive_label = STAT_FONT.render("Alive: " + str(len(birds)),1,(255,255,255))
+        SCREEN.blit(alive_label, (10, 50))
+
         
+        
+
         #Updating display
         pygame.display.update()
         clock.tick(FPS)
